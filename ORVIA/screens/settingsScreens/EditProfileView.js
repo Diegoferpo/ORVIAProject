e6b36cf7af90b1435898ad79a6fbd73c23f4cdc6
Emmanuel Moscoso <emoscoso21@alumnos.uaq.mx>
2025-05-27 15:43:03 -0600

    feat:save changes

--- ORVIA/screens/settingsScreens/EditProfileView.js
+++ ORVIA/screens/settingsScreens/EditProfileView.js
@@ -13,7 +13,7 @@ import DatePickerComponent from '../../components/DatePickerComponent';
 
 const EditProfileView = ({ navigation }) => {
   const [firstName, setFirstName] = useState('Emmet');
-  const [lastName, setLastName] = useState('Lego');
+  const [lastName, setLastName] = useState('L.');
   const [profession, setProfession] = useState('Fisioterapeuta');
   const [phone, setPhone] = useState('4421234567');
 
